d41700342850135ab303bbb50f898ebb535e7c82
Amzy <AS200204@reigate.ac.uk>
2020-11-11 16:29:37 +0000

    Making the user enter ten numbers and storing them

---
+++ src/hw8_sorting_numbers/HW8_Sorting_Numbers.java
@@ -0,0 +1,45 @@
+//Sorting Numbers:
+
+//Stores a series of numbers inputted from the user 
+//Sort the numbers into ascending order and display them 
+//Find the avergae, minimum and maximum numbers, display them 
+
+package hw8_sorting_numbers;
+
+import java.util.Scanner;
+
+public class HW8_Sorting_Numbers {
+
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); 
+        
+        //Ask the user to input a set of numbers:
+        
+        System.out.println("You will enter 10 WHOLE numbers"); 
+        
+        int ask = 0; 
+        
+        while(ask < 10){  
+            System.out.println("");
+            System.out.println("Please input a WHOLE number and press enter"); 
+            int userInput = input.nextInt(); 
+            ask = ask + 1; 
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
